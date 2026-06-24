fef0d01dc2ec1f0cb843c03e26af74ab6f8e0789
Ryan Abdaulaziz <ryanabdaulaziz@Ryans-MacBook-Air.local>
2026-06-24 12:24:07 +0300

    test

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     secret_key: str
     algorithm: str
-    access_token_expire_minutes: int
+    access_token_expire_minutes: int = 30
     database_name: str
     database_username: str
     database_password: str
